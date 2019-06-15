e2fd2858d4ad96d1e6de78db66cc08fce617121b
Kristiyan Krastev <kristiyan.krastev@primeholding.com>
2019-06-15 20:38:17 +0300

    created simple recursive folder generator that takes the folder structure from a json

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 const Configuration = require('./lib/configuration/configuration');
 const yargs = require('yargs');
-// will take site name as a first argument
-
 
 Configuration.init(yargs);
